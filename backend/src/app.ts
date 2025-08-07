e660fab3e7ee80970b96ac5b26a84e9812de2aab
Gustavo Vínicius <164204886+gutsgon@users.noreply.github.com>
2025-08-07 01:02:01 -0300

    docs: Update cors origins

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,8 @@ import swaggerUi from 'swagger-ui-express'
 // Inicialização e configuração da API
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: ['https://front-end-production-c050.up.railway.app/tarefas', '.railway.app'],
+methods: ['GET', 'POST','PUT', 'DELETE', 'OPTIONS']}));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(routes)
 
